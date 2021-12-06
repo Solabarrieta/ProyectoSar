548e6e1743fb6bf8281c752af3e14e0f29383bf8
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-12-06 19:15:16 +0100

    agregando singup

---
+++ .history/php/SingUp_20211206172433.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h>Login</h>
+    <div class="container">
+        <form action="" method="post" class="form">
+            input:text
+            <input type="email" name="" id="email__input">
+            <label for="email__input"></label>
+        </form>
+    </div>
+    
+</body>
+</html>
---
+++ .history/php/SingUp_20211206172555.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h>Login</h>
+    <div class="container">
+        <form action="" method="post" class="form">
+            <input type="text" name="" id="username" class="form__imput">
+            <label for="username" class="form__label"></label>
+            <input type="email" name="" id="email">
+            <label for="email"></label>
+            <input type="password" name="password" id="">
+        </form>
+    </div>
+    
+</body>
+</html>
