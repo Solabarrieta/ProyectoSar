0c3d74c136d2dac69f6b9c271dff13c7feaa1e0d
Aleina <sviatosana@gmail.com>
2021-12-07 21:38:08 +0100

    Intento merge

---
+++ php/index.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Login</title>
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'>
+        <link rel="stylesheet" href="styleHome.css" />
+        <script src="scriptHome.js"></script>
+    </head>
+    <body>
+        <nav class="container">
+            <div class="logo"> <i class="far fa-sticky-note"></i> Mis Notas </div>
+            <ul>
+                <li><a href="index.php" class="selected">Página principal</a></li>
+                <li><a href="Login.php">Login</a></li>
+                <li><a href="SignUp.php">Sign Up</a></li>
+            </ul>
+        </nav>
+        <hr>
+        <div class="home">
+            <h1>Hola</h1>
+        </div>
+    </body>
+</html>
