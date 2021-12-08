54b0a55cb2fe7a8bd45c8d664313351fb5137eb1
Oier <oieruni22@gmail.com>
2021-12-08 19:42:36 +0100

    Modificando las tarjetas, poniendo un input para introducir el titulo de las notas y la categoria

--- js/notas.js
+++ js/notas.js
@@ -1,40 +1,44 @@
-const addButton = document.getElementById('add');
-
+const addButton = document.getElementById("add");
 
 $(document).ready(() => {
-    $.get('../xml/Notes.xml', (xml) => {
-        $(xml)
-            .find('NoteUser')
-            .each(function viewdata() {
-                addNote($(this).text(),$(this).find('Text').attr("title"),$(this).find('Text').attr('categoria'));
-            });
-    });
+  $.get("../xml/Notes.xml", (xml) => {
+    $(xml)
+      .find("NoteUser")
+      .each(function viewdata() {
+        addNote(
+          $(this).text(),
+          $(this).find("Text").attr("title"),
+          $(this).find("Text").attr("categoria")
+        );
+      });
+  });
 
-    addButton.addEventListener("click", () => {
-        //console.log('pulsado');
-        addNote();
-    });
-}
-);
+  addButton.addEventListener("click", () => {
+    //console.log('pulsado');
+    addNote();
+  });
+});
 
 function addNote(text = " ", title = " ", categoria = "") {
+  const note = document.createElement("div");
+  note.classList.add("note");
 
-    const note = document.createElement("div");
-    note.classList.add("note");
-
-    note.innerHTML = `
+  note.innerHTML = `
         <div class="notes">
             <div class="tools">
-                <h1>${title ? title : "Título"}</h1>
+                <input type= "text" placeholder="Titulo" class="input-titulo"> 
                 <div class="div__buttons">
-                    <p class = "categoria">${categoria ? categoria : "Generico"}<p/>
+
+                    <input type="text" placeholder="Categoria" class="categoria">
                     <div class="buttons">
                         <button class="edit"><i class="fas fa-edit"></i></button>
                         <button class="delete"><i class="fas fa-trash-alt"></i></button>
                     </div>
                 </div>
             </div>
-            <div class="main ${text ? "" : "hidden"}"><h1>fjdskljfasdlk</h1></div>
+            <div class="main ${
+              text ? "" : "hidden"
+            }"><h1>fjdskljfasdlk</h1></div>
             
             <input:text></input>
             <textarea class="${text ? "hidden" : ""}">
@@ -42,29 +46,28 @@ function addNote(text = " ", title = " ", categoria = "") {
             </textarea>
         </div>
     `;
-    const main = note.querySelector(".main");
-    const textArea = note.querySelector("textarea");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
 
-    const editBtn = note.querySelector(".edit");
-    const deleteBtn = note.querySelector(".delete");
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
 
-    textArea.value = text;
-    main.innerHTML=text;
+  textArea.value = text;
+  main.innerHTML = text;
 
-    editBtn.addEventListener("click", () => {
-        console.log("ha pulsado el botón de editar");
-        main.classList.toggle("hidden");
-        textArea.classList.toggle("hidden");
+  editBtn.addEventListener("click", () => {
+    console.log("ha pulsado el botón de editar");
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
 
-    });
+  deleteBtn.addEventListener("click", () => {
+    console.log("ha pulsado el botón de borrar");
+    note.remove();
+    //updateLS();
+  });
 
-    deleteBtn.addEventListener("click", () => {
-        console.log("ha pulsado el botón de borrar");
-        note.remove();
-        //updateLS();
-    });
-
-    textArea.value = text;
-    main.innerHTML = text;
-    document.body.appendChild(note);
+  textArea.value = text;
+  main.innerHTML = text;
+  document.body.appendChild(note);
 }
